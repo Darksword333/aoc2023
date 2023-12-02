ed320145d5338927d6b817efc27825c17f2f2da6
Gabriel <49925197+Darksword333@users.noreply.github.com>
2023-12-02 13:47:45 +0000

    Day 1 ended

--- Day1/python/day1_part2.py
+++ Day1/python/day1_part2.py
@@ -1,48 +1,33 @@
-with open("input.txt", "r") as f:
+with open("Day1/input.txt", "r") as f:
     data = f.readlines()
 
-liste_num = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-num_char = {"one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9}
-
 result = 0
-reconstructed_str = ""
-first_digit = 0
-last_digit = 0
-
 for line in data:
+    line=line.replace("one", "o1ne")
+    line=line.replace("two", "t2wo")
+    line=line.replace("three", "t3hree")
+    line=line.replace("four", "f4our")
+    line=line.replace("five", "f5ive")
+    line=line.replace("six", "s6ix")
+    line=line.replace("seven", "s7even")
+    line=line.replace("eight", "e8ight")
+    line=line.replace("nine", "n9ine")
+    line=line.replace("zero", "z0ero")
 
     for char in line:
-        reconstructed_str += char
         if char.isdigit():
-            print("ici1", char)
             first_digit = int(char)
-            reconstructed_str = ""
             break
-        elif not char.isdigit():
-            for number in liste_num:
-                if number in reconstructed_str:
-                    print("la1", number)
-                    first_digit = num_char[number]
-                    reconstructed_str = ""
-                    break
                     
-    for char in line:
-        reconstructed_str += char
+    for char in reversed(line):
         if char.isdigit():
-            print("ici", char)
             last_digit = int(char)
-            reconstructed_str = ""
-        else:
-            for number in liste_num:
-                if number in reconstructed_str:
-                    print("la", number)
-                    last_digit = num_char[number]
-                    reconstructed_str = ""
+            break
 
-    number = int(str(first_digit) + str(last_digit))
+    num = int(str(first_digit) + str(last_digit))
     first_digit = 0
     last_digit = 0
-
-    result += number
+    result += num
+    num = 0
 
 print(result)
